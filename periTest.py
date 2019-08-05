c21646627997f468e75a678c60cd6cacfcbbbfd7
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-08-05 17:11:27 +0100

    periTest.py now works. Changed backend and add tkinter to make plot show
    Still not interactive like it should be though. I also had to edit the source code of peri to fix it after a matplotlib update. This has been reported as an issue on the Git page

--- periTest.py
+++ periTest.py
@@ -1,4 +1,11 @@
+import matplotlib
+matplotlib.rcParams["backend"]="TkAgg"
 from peri import util
 from peri.viz.interaction import OrthoViewer
-im = util.RawImage('Downloads/small_confocal_image.tif')
+im = util.RawImage('../../Downloads/small_confocal_image.tif')
 OrthoViewer(im.get_image())
+
+print(matplotlib.get_backend())
+import Tkinter
+from Tkinter import *
+Tk().mainloop()
