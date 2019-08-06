66e1e0c31c459492c87bdec5cd6444f951d5b37f
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-08-06 16:37:46 +0100

    General fiddling with peri, including now using the master code from git

--- periTest.py
+++ periTest.py
@@ -1,11 +1,14 @@
 import matplotlib
-matplotlib.rcParams["backend"]="TkAgg"
+matplotlib.rcParams["backend"]="TkAgg" #with OrthoViewer this gives an error that 'this isn't a Tk application'
 from peri import util
 from peri.viz.interaction import OrthoViewer
 im = util.RawImage('../../Downloads/small_confocal_image.tif')
 OrthoViewer(im.get_image())
 
-print(matplotlib.get_backend())
-import Tkinter
-from Tkinter import *
-Tk().mainloop()
+matplotlib.pyplot.show(block = True)
+
+
+# print(matplotlib.get_backend())
+# import Tkinter
+# from Tkinter import *
+# Tk().mainloop()
