d52ff05e4798038f4410f4260c4d3ee1886f2084
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-08-30 17:47:29 +0100

    Adapted periTest.py to look at my data. Currently it doesn't work.
    ---------------------------------------------------------------------------
    TypeError                                 Traceback (most recent call last)
    <ipython-input-68-1433e5b646d3> in <module>
    ----> 1 st = states.ImageState(im, [particles, illumination, background, point_spread_function, offset], mdl=model)
    
    ~/anaconda3/lib/python3.7/site-packages/peri-0.1.2-py3.7.egg/peri/states.py in __init__(self, image, comps, mdl, sigma, priors, pad, model_as_data)
        547         self.model_as_data = model_as_data
        548 
    --> 549         comp.ComponentCollection.__init__(self, comps=comps)
        550 
        551         self.set_model(mdl=mdl)
    
    ~/anaconda3/lib/python3.7/site-packages/peri-0.1.2-py3.7.egg/peri/comp/comp.py in __init__(self, comps, field_reduce_func, category)
        378         self.field_reduce_func = field_reduce_func
        379 
    --> 380         self.setup_params()
        381         self.setup_passthroughs()
        382         self._parent = None
    
    ~/anaconda3/lib/python3.7/site-packages/peri-0.1.2-py3.7.egg/peri/comp/comp.py in setup_params(self)
        399 
        400         for c in self.comps:
    --> 401             c.register(self)
        402 
        403             if not isinstance(c, (Component, ComponentCollection)):
    
    TypeError: register() missing 1 required positional argument: 'obj'

--- periTest.py
+++ periTest.py
@@ -1,14 +1,35 @@
 import matplotlib
-matplotlib.rcParams["backend"]="TkAgg" #with OrthoViewer this gives an error that 'this isn't a Tk application'
+# matplotlib.rcParams["backend"]="TkAgg"
 from peri import util
-from peri.viz.interaction import OrthoViewer
-im = util.RawImage('../../Downloads/small_confocal_image.tif')
-OrthoViewer(im.get_image())
+import numpy as np
+from peri.comp import objs
+from peri.viz.interaction import OrthoPrefeature
+from peri import models
+from peri import states
+from peri import comp
 
-matplotlib.pyplot.show(block = True)
+im = util.RawImage('/Volumes/PhD/expDesign/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif')
 
+tile = util.Tile([0,312,0],right=[12,512,200])
+small_im = util.RawImage('/Volumes/PhD/expDesign/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif', tile=tile)
 
-# print(matplotlib.get_backend())
-# import Tkinter
-# from Tkinter import *
-# Tk().mainloop()
+particle_positions = np.load('/Volumes/PhD/expDesign/Python/particles1.npy')
+particle_radii = 8.0
+particles = objs.PlatonicSpheresCollection(particle_positions, particle_radii)
+# imaginary_slide = objs.Slab(zpos=6)
+
+# objects = comp.comp.ComponentCollection([particles], category='obj')
+
+illumination = comp.ilms.LegendrePoly2P1D()
+
+background = comp.ilms.LegendrePoly2P1D(category='bkg')
+
+point_spread_function = comp.psfs.AnisotropicGaussian
+
+offset = comp.comp.GlobalScalar(name='offset', value=0.)
+
+# OrthoPrefeature(im.get_image(), particle_positions, viewrad=3.0)
+
+model = models.ConfocalImageModel()
+
+st = states.ImageState(im, [particles, illumination, background, point_spread_function, offset], mdl=model)
