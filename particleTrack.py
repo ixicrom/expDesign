e75927f877ed22133ad71dc33318a819a8da2991
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-09-05 16:33:34 +0100

    updated particleTrack.py for small part of image & to plot all particles
    All particles plotted on one image slice, particle locations colour coded based on z

--- particleTrack.py
+++ particleTrack.py
@@ -10,11 +10,18 @@ import trackpy as tp
 
 from skimage import io
 
-image=pims.TiffStack('../2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif')
-im = io.imread('../2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif', as_gray=True)
+# image=pims.TiffStack('/Volumes/PhD/expDesign/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif')
+im = io.imread('/Volumes/PhD/expDesign/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif', as_gray=True)
 im_array = np.array(im)
-
+im.shape
+im_array_small = im_array[:,312:, 0:200]
+im_array_small.shape
 f=tp.locate(im_array,diameter=(3,11,11))
+f_small = tp.locate(im_array_small,diameter=(3,11,11))
 
-plt.figure()
-tp.annotate3d(f[f['z']==f['z']],im_array)
+# plt.figure()
+# tp.annotate3d(f[f['z']==f['z']],im_array)
+plt.imshow(im_array_small[2,:,:],cmap="gray")
+scatter = plt.scatter(x=f_small["x"],y=f_small["y"], c=f_small["z"],marker="x")
+plt.legend(*scatter.legend_elements(), title="z")
+plt.show()
