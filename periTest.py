2ae06cce43c19c49dab36114b8cd9b8c26eea007
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-09-05 12:22:25 +0100

    Tried Jason's parameters in periTest.py
    Still getting the same error, but I should start from this point I think

--- periTest.py
+++ periTest.py
@@ -21,34 +21,49 @@ particle_radii = 8.0
 particles = objs.PlatonicSpheresCollection(particle_positions, particle_radii)
 # imaginary_slide = objs.Slab(zpos=6)
 
-# objects = comp.comp.ComponentCollection([particles], category='obj')
+objects = comp.comp.ComponentCollection([particles], category='obj')
 
-illumination = comp.ilms.Polynomial3D(order=(1,1,1))
-# illumination = comp.ilms.BarnesStreakLegPoly2P1D(npts=(16, 10, 8, 4), zorder=8)
+# illumination = comp.ilms.Polynomial3D(order=(1,1,1))
+# # illumination = comp.ilms.BarnesStreakLegPoly2P1D(npts=(16, 10, 8, 4), zorder=8)
+#
+# # background = comp.ilms.LegendrePoly2P1D(category='bkg')
+# background = comp.ilms.LegendrePoly2P1D(order=(7,2,2), category='bkg')
+#
+# # point_spread_function = comp.psfs.AnisotropicGaussian
+# point_spread_function = comp.exactpsf.FixedSSChebLinePSF()
+#
+# offset = comp.comp.GlobalScalar(name='offset', value=0.)
+# # offset = comp.GlobalScalar(name='offset', value=0.)
+#
+# # OrthoPrefeature(im.get_image(), particle_positions, viewrad=3.0)
+#
+# model = models.ConfocalImageModel()
+#
+# st = states.ImageState(small_im, [particles, illumination, background, point_spread_function, offset], mdl=model)
+#
+# runner.link_zscale(st)
+# print(st.error)
+#
+# runner.optimize_from_initial(st)
+#
+#
+# tile2 = util.Tile([11,312,0],right=[12,512,200])
+# tile2
+# plots.compare_data_model_residuals(st, tile=tile2)
+#
+# plots.examine_unexplained_noise(st)
+zpixel=0.82
+xpixel=35.2/512
+zpixel/xpixel
 
-# background = comp.ilms.LegendrePoly2P1D(category='bkg')
-background = comp.ilms.LegendrePoly2P1D(order=(7,2,2), category='bkg')
-
-# point_spread_function = comp.psfs.AnisotropicGaussian
-point_spread_function = comp.exactpsf.FixedSSChebLinePSF()
-
-offset = comp.comp.GlobalScalar(name='offset', value=0.)
-# offset = comp.GlobalScalar(name='offset', value=0.)
-
-# OrthoPrefeature(im.get_image(), particle_positions, viewrad=3.0)
-
-model = models.ConfocalImageModel()
-
-st = states.ImageState(small_im, [particles, illumination, background, point_spread_function, offset], mdl=model)
-
+background = comp.ilms.LegendrePoly2P1D(order=(3,2,2), category='bkg')
+illumination = comp.ilms.BarnesStreakLegPoly2P1D(npts=(4, 2, 2))
+offset = comp.GlobalScalar(name='offset', value=0.)
+point_spread_function = comp.exactpsf.FixedSSChebLinePSF(pxsize=xpixel)
+model = models.ConfocalDyedParticlesModel()
+st = states.ImageState(small_im, [objects, illumination, background, point_spread_function], mdl=model)
+st.update('zscale', zpixel/xpixel)
+st.update('psf-laser-wavelength', .488)
 runner.link_zscale(st)
-print(st.error)
 
 runner.optimize_from_initial(st)
-
-
-tile2 = util.Tile([11,312,0],right=[12,512,200])
-tile2
-plots.compare_data_model_residuals(st, tile=tile2)
-
-plots.examine_unexplained_noise(st)
