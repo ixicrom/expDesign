4f45769f9e471fdec9481daa5dc9496b0130e2ea
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-09-20 16:39:39 +0100

    gPyOpt_test.py trying it out

---
+++ gPyOpt_test.py
@@ -0,0 +1,10 @@
+import GPyOpt
+from GPyOpt.methods import BayesianOptimization
+import numpy as np
+
+def f(x): return (6*x-2)**2*np.sin(12*x-4)
+domain = [{'name': 'var_1', 'type': 'continuous', 'domain': (0,1)}]
+
+myBopt = BayesianOptimization(f=f, domain=domain)
+myBopt.run_optimization(max_iter=5)
+myBopt.plot_acquisition()
