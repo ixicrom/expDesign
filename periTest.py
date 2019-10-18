30a480053e78239fc3360d88ca358443ef9dd822
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-10-18 14:40:26 +0100

    periTest.py works for a small slice of the image but not the whole thing

--- periTest.py
+++ periTest.py
@@ -10,52 +10,26 @@ from peri import comp
 from peri import runner
 from peri.viz import plots
 
+from datetime import datetime
 
-im = util.RawImage('/Volumes/PhD/expDesign/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_RhB_xyz007_x100_z35_K4.tif')
+im = util.RawImage('/Volumes/PhD/expDesign/Data/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif')
 
 tile = util.Tile([5,312,0],right=[12,512,200])
-tile
-small_im = util.RawImage('/Volumes/PhD/expDesign/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_RhB_xyz007_x100_z35_K4.tif', tile=tile)
+small_im = util.RawImage('/Volumes/PhD/expDesign/Data/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif', tile=tile)
+zpixel=0.82
+xpixel=35.2/512
+zscale=zpixel/xpixel
 
 particle_positions = np.load('part_loc_smallSlice.npy')
 particle_radii = 8.0
-particles = objs.PlatonicSpheresCollection(particle_positions, particle_radii)
+particles = objs.PlatonicSpheresCollection(particle_positions, particle_radii, zscale=zscale)
 # imaginary_slide = objs.Slab(zpos=6)
-
+print(particles)
 objects = comp.comp.ComponentCollection([particles], category='obj')
 
-# illumination = comp.ilms.Polynomial3D(order=(1,1,1))
-# # illumination = comp.ilms.BarnesStreakLegPoly2P1D(npts=(16, 10, 8, 4), zorder=8)
-#
-# # background = comp.ilms.LegendrePoly2P1D(category='bkg')
-# background = comp.ilms.LegendrePoly2P1D(order=(7,2,2), category='bkg')
-#
-# # point_spread_function = comp.psfs.AnisotropicGaussian
-# point_spread_function = comp.exactpsf.FixedSSChebLinePSF()
-#
-# offset = comp.comp.GlobalScalar(name='offset', value=0.)
-# # offset = comp.GlobalScalar(name='offset', value=0.)
-#
-# # OrthoPrefeature(im.get_image(), particle_positions, viewrad=3.0)
-#
-# model = models.ConfocalImageModel()
-#
-# st = states.ImageState(small_im, [particles, illumination, background, point_spread_function, offset], mdl=model)
-#
-# runner.link_zscale(st)
-# print(st.error)
-#
-# runner.optimize_from_initial(st)
-#
-#
-# tile2 = util.Tile([11,312,0],right=[12,512,200])
-# tile2
-# plots.compare_data_model_residuals(st, tile=tile2)
-#
-# plots.examine_unexplained_noise(st)
-zpixel=0.82
-xpixel=35.2/512
-zpixel/xpixel
+# OrthoPrefeature(im.get_image(), particle_positions, viewrad=3.0)
+
+
 
 background = comp.ilms.LegendrePoly2P1D(order=(4,2,2), category='bkg')
 illumination = comp.ilms.BarnesStreakLegPoly2P1D(npts=(4, 2, 2))
@@ -63,9 +37,13 @@ offset = comp.GlobalScalar(name='offset', value=0.)
 point_spread_function = comp.exactpsf.FixedSSChebLinePSF(pxsize=xpixel)
 model = models.ConfocalDyedParticlesModel()
 st = states.ImageState(small_im, [objects, illumination, background, point_spread_function], mdl=model)
-st.update('zscale', zpixel/xpixel)
+st.update('zscale', zscale)
+savefile = "/Volumes/PhD/expDesign/states/"+datetime.now().strftime("%Y%m%d-%H%M%S") + "_unoptimized"
+states.save(st,savefile)
 # st.update('psf-laser-wavelength', .488)
 runner.link_zscale(st)
 
 runner.optimize_from_initial(st)
+savefile = "/Volumes/PhD/expDesign/states/"+datetime.now().strftime("%Y%m%d-%H%M%S") + "_inital_optimized"
+states.save(st,savefile)
 runner.finish_state(st)
