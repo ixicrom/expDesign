99c8924d89dceb9a6e580d3e2a387ba4d2a448c9
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-11-06 16:23:17 +0000

    streamlines peri_and_track_altImage.py

--- peri_and_track_altImage.py
+++ peri_and_track_altImage.py
@@ -14,34 +14,39 @@ import pandas as pd
 from pandas import DataFrame, Series
 import pims
 import trackpy as tp
-from skimage import io
+# from skimage import io
 from datetime import datetime
 
-im = io.imread('/Volumes/PhD/expDesign/Data/David/6-5a.tif')
-im_array = np.array(im)
+imFile = '/Volumes/PhD/expDesign/Data/David/6-5a.tif'
+# im = io.imread(imFile)
+# im_array = np.array(im)
 # print(im_array.shape)
 # im_array_small = im_array[5:,312:, 0:200]
-# im_array_small.shape
-f=tp.locate(im_array,diameter=(9,9,9))
-f[f['z']==f['z']]
-# f_small = tp.locate(im_array_small,diameter=(3,11,11))
+# im_array_small
+# f=tp.locate(im_array,diameter=(9,9,9))
+# f[f['z']==f['z']]
+# f_small = tp.locate(im_array_small,diameter=(9,9,9))
 # f_small=f_small[f_small['z']==f_small['z']]
 # np.save('part_loc_davidImage.npy', np.array(f[f.columns[0:3,]]))
-particle_positions = np.array(f[f.columns[0:3,]])
+# particle_positions = np.array(f_small[f_small.columns[0:3,]])
 
-raw_im = util.RawImage('/Volumes/PhD/expDesign/Data/David/6-5a.tif')
+raw_im = util.RawImage(imFile)
+im_arr = raw_im.get_image()
+f=tp.locate(im_arr,diameter=(9,9,9))
 
-# tile = util.Tile([5,312,0],right=[12,512,200])
-# small_im = util.RawImage('/Volumes/PhD/expDesign/Data/2011_DAS_SoftMatter_Data/jtLLF090701_BR090729_NBD_xyz007_x100_z35_K4.tif', tile=tile)
+tile = util.Tile(200)
+small_im = util.RawImage(imFile, tile=tile)
+small_im_arr = small_im.get_image()
+f_small = tp.locate(small_im_arr, diameter=(9,9,9))
+particle_positions = np.array(f_small[f_small.columns[0:3,]])
+
 zpixel=1
 xpixel=1
 zscale=zpixel/xpixel
 
-# particle_positions = np.load('part_loc_smallSlice.npy')
 particle_radii = 8.0
 particles = objs.PlatonicSpheresCollection(particle_positions, particle_radii, zscale=zscale)
-# imaginary_slide = objs.Slab(zpos=6)
-print(particles)
+
 objects = comp.comp.ComponentCollection([particles], category='obj')
 
 # OrthoPrefeature(im.get_image(), particle_positions, viewrad=3.0)
@@ -53,7 +58,7 @@ illumination = comp.ilms.BarnesStreakLegPoly2P1D(npts=(4, 2, 2))
 offset = comp.GlobalScalar(name='offset', value=0.)
 point_spread_function = comp.exactpsf.FixedSSChebLinePSF(pxsize=xpixel)
 model = models.ConfocalDyedParticlesModel()
-st = states.ImageState(raw_im, [objects, illumination, background, point_spread_function], mdl=model)
+st = states.ImageState(small_im, [objects, illumination, background, point_spread_function], mdl=model)
 st.update('zscale', zscale)
 savefile = "/Volumes/PhD/expDesign/states/David"+datetime.now().strftime("%Y%m%d-%H%M%S") + "_unoptimized"
 states.save(st,savefile)
